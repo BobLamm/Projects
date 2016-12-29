f37e37a009ec32f6dfc37f8b51a52fbe6fac66c1
Bob Lamm <robert.e.lamm@gmail.com>
2016-12-29 15:45:18 -0500

    Further filling in of details...
    Added ability to recall camera presets on buttons panel
    Defined Clips/Supers/Stills menu functionality
    Made Button Settings panel better and controlled editability
    Took out old temporary diagnostic code
    Added copy of current configuration so it works on Fred's machine
    Clarified variable names to make things clearer and more consistent

--- VGV101/VGV101/DlgButtonSettings.Designer.cs
+++ VGV101/VGV101/DlgButtonSettings.Designer.cs
@@ -28,21 +28,22 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
             this.button18 = new System.Windows.Forms.Button();
             this.buttonsData = new System.Windows.Forms.DataGridView();
+            this.enableEditingButton = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.buttonsData)).BeginInit();
             this.SuspendLayout();
             // 
             // button18
             // 
             this.button18.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.button18.Location = new System.Drawing.Point(1484, 678);
+            this.button18.Location = new System.Drawing.Point(1653, 713);
             this.button18.Name = "button18";
-            this.button18.Size = new System.Drawing.Size(390, 67);
+            this.button18.Size = new System.Drawing.Size(219, 35);
             this.button18.TabIndex = 63;
-            this.button18.Text = "Update Buttons";
+            this.button18.Text = "Update Buttons (Even more dangerous)";
             this.button18.UseVisualStyleBackColor = true;
             this.button18.Click += new System.EventHandler(this.button18_Click);
             // 
@@ -55,37 +56,50 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.buttonsData.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
             this.buttonsData.BackgroundColor = System.Drawing.SystemColors.Control;
-            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.ControlLight;
-            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
-            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
-            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
-            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
-            this.buttonsData.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle3.BackColor = System.Drawing.SystemColors.ControlLight;
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle3.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.buttonsData.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle3;
             this.buttonsData.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle2.BackColor = System.Drawing.Color.White;
-            dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.ControlText;
-            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
-            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
-            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
-            this.buttonsData.DefaultCellStyle = dataGridViewCellStyle2;
+            dataGridViewCellStyle4.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle4.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle4.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle4.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle4.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle4.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.buttonsData.DefaultCellStyle = dataGridViewCellStyle4;
             this.buttonsData.EnableHeadersVisualStyles = false;
             this.buttonsData.Location = new System.Drawing.Point(10, 16);
             this.buttonsData.Name = "buttonsData";
+            this.buttonsData.ReadOnly = true;
             this.buttonsData.RowHeadersVisible = false;
             this.buttonsData.RowTemplate.Height = 25;
             this.buttonsData.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.CellSelect;
-            this.buttonsData.Size = new System.Drawing.Size(1862, 645);
+            this.buttonsData.Size = new System.Drawing.Size(1862, 679);
             this.buttonsData.TabIndex = 62;
+            // 
+            // enableEditingButton
+            // 
+            this.enableEditingButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.enableEditingButton.Location = new System.Drawing.Point(1464, 713);
+            this.enableEditingButton.Name = "enableEditingButton";
+            this.enableEditingButton.Size = new System.Drawing.Size(170, 35);
+            this.enableEditingButton.TabIndex = 64;
+            this.enableEditingButton.Text = "Enable Editing (Dangerous!)";
+            this.enableEditingButton.UseVisualStyleBackColor = true;
+            this.enableEditingButton.Click += new System.EventHandler(this.enableEditingButton_Click);
             // 
             // DlgButtonSettings
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1884, 761);
+            this.Controls.Add(this.enableEditingButton);
             this.Controls.Add(this.button18);
             this.Controls.Add(this.buttonsData);
             this.Name = "DlgButtonSettings";
@@ -99,5 +113,6 @@
 
         private System.Windows.Forms.Button button18;
         private System.Windows.Forms.DataGridView buttonsData;
+        private System.Windows.Forms.Button enableEditingButton;
     }
 }
