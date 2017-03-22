75f0702b2461f3c8c4e1ba1c5a4c1ff41debbb09
Fred Koschara <github@L5Software.com>
2017-03-22 17:54:08 -0400

    Add preview buttons to main window design

--- Video_Test_Fixture/Video_Test_Fixture/MainWindow.Designer.cs
+++ Video_Test_Fixture/Video_Test_Fixture/MainWindow.Designer.cs
@@ -35,12 +35,15 @@
             this.addTextButton = new System.Windows.Forms.Button();
             this.connectToCameras = new System.Windows.Forms.Button();
             this.pictureBoxForVideo = new AxAXISMEDIACONTROLLib.AxAxisMediaControl();
+            this.btnPreview1 = new System.Windows.Forms.Button();
+            this.btnPreview2 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxForVideo)).BeginInit();
             this.SuspendLayout();
             // 
             // camera1Button
             // 
-            this.camera1Button.Location = new System.Drawing.Point(412, 64);
+            this.camera1Button.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.camera1Button.Location = new System.Drawing.Point(413, 124);
             this.camera1Button.Name = "camera1Button";
             this.camera1Button.Size = new System.Drawing.Size(128, 23);
             this.camera1Button.TabIndex = 1;
@@ -50,7 +53,8 @@
             // 
             // camera2Button
             // 
-            this.camera2Button.Location = new System.Drawing.Point(412, 93);
+            this.camera2Button.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.camera2Button.Location = new System.Drawing.Point(413, 153);
             this.camera2Button.Name = "camera2Button";
             this.camera2Button.Size = new System.Drawing.Size(128, 23);
             this.camera2Button.TabIndex = 2;
@@ -60,7 +64,8 @@
             // 
             // addGraphicButton
             // 
-            this.addGraphicButton.Location = new System.Drawing.Point(412, 144);
+            this.addGraphicButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.addGraphicButton.Location = new System.Drawing.Point(413, 199);
             this.addGraphicButton.Name = "addGraphicButton";
             this.addGraphicButton.Size = new System.Drawing.Size(128, 23);
             this.addGraphicButton.TabIndex = 3;
@@ -70,7 +75,8 @@
             // 
             // addTextButton
             // 
-            this.addTextButton.Location = new System.Drawing.Point(412, 173);
+            this.addTextButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.addTextButton.Location = new System.Drawing.Point(413, 228);
             this.addTextButton.Name = "addTextButton";
             this.addTextButton.Size = new System.Drawing.Size(128, 23);
             this.addTextButton.TabIndex = 4;
@@ -80,7 +86,8 @@
             // 
             // connectToCameras
             // 
-            this.connectToCameras.Location = new System.Drawing.Point(412, 12);
+            this.connectToCameras.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.connectToCameras.Location = new System.Drawing.Point(413, 12);
             this.connectToCameras.Name = "connectToCameras";
             this.connectToCameras.Size = new System.Drawing.Size(128, 23);
             this.connectToCameras.TabIndex = 5;
@@ -98,11 +105,35 @@
             this.pictureBoxForVideo.TabIndex = 6;
             this.pictureBoxForVideo.OnClick += new AxAXISMEDIACONTROLLib._IAxisMediaControlEvents_OnClickEventHandler(this.pictureBoxForVideo_Click);
             // 
+            // btnPreview1
+            // 
+            this.btnPreview1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnPreview1.Location = new System.Drawing.Point(413, 53);
+            this.btnPreview1.Name = "btnPreview1";
+            this.btnPreview1.Size = new System.Drawing.Size(128, 23);
+            this.btnPreview1.TabIndex = 7;
+            this.btnPreview1.Text = "Preview Camera 1";
+            this.btnPreview1.UseVisualStyleBackColor = true;
+            this.btnPreview1.Click += new System.EventHandler(this.btnPreview1_Click);
+            // 
+            // btnPreview2
+            // 
+            this.btnPreview2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnPreview2.Location = new System.Drawing.Point(413, 83);
+            this.btnPreview2.Name = "btnPreview2";
+            this.btnPreview2.Size = new System.Drawing.Size(128, 23);
+            this.btnPreview2.TabIndex = 8;
+            this.btnPreview2.Text = "Preview Camera 2";
+            this.btnPreview2.UseVisualStyleBackColor = true;
+            this.btnPreview2.Click += new System.EventHandler(this.btnPreview2_Click);
+            // 
             // MainWindow
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(584, 330);
+            this.ClientSize = new System.Drawing.Size(553, 300);
+            this.Controls.Add(this.btnPreview2);
+            this.Controls.Add(this.btnPreview1);
             this.Controls.Add(this.pictureBoxForVideo);
             this.Controls.Add(this.connectToCameras);
             this.Controls.Add(this.addTextButton);
@@ -113,7 +144,6 @@
             this.Name = "MainWindow";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Video_Test_Fixture";
-            this.Load += new System.EventHandler(this.MainWindow_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxForVideo)).EndInit();
             this.ResumeLayout(false);
 
@@ -126,5 +156,7 @@
         private System.Windows.Forms.Button addTextButton;
         private System.Windows.Forms.Button connectToCameras;
         private AxAXISMEDIACONTROLLib.AxAxisMediaControl pictureBoxForVideo;
+        private System.Windows.Forms.Button btnPreview1;
+        private System.Windows.Forms.Button btnPreview2;
     }
 }
