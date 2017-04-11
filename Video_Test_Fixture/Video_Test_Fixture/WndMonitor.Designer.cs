1a46152c95752c50dfb112dd71e09b545b657276
Fred Koschara <github@L5Software.com>
2017-04-10 20:26:56 -0400

    WndPreview renamed to WndMonitor
    Any other changes masked by git not knowing how to deal with renamed
    files

---
+++ Video_Test_Fixture/Video_Test_Fixture/WndMonitor.Designer.cs
@@ -0,0 +1,36 @@
+﻿namespace Video_Test_Fixture
+{
+    partial class WndMonitor
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(WndMonitor));
+            this.SuspendLayout();
+            // 
+            // WndPreview
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(640, 360);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "WndPreview";
+            this.Text = "WndPreview";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.WndPreview_FormClosing);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
