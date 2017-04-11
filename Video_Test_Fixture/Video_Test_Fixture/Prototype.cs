7bd6a37a12fbd68d3215952bcfffd684e3be0a66
Fred Koschara <github@L5Software.com>
2017-04-10 20:25:09 -0400

    Prototype dates updated

--- Video_Test_Fixture/Video_Test_Fixture/Prototype.cs
+++ Video_Test_Fixture/Video_Test_Fixture/Prototype.cs
@@ -10,15 +10,16 @@
 *
 *	Author:			Fred Koschara
 *	Proto Created:	December tenth, 2016
-*	Creation Date:	March nineteenth, 2017
-*	Creation Date:	March 19, 2017
-*	Last Modified:	March 19, 2017 @ 4:01 pm
+*	Creation Date:	April seventh, 2017
+*	Creation Date:	April 7, 2017
+*	Last Modified:	April 7, 2017 @ 8:30 pm
 *
 *	Revision History:
 *	   Date		  by		Description
-*	2017/03/19	wfredk	original development
+*	2017/04/07	wfredk	original development
 *		|						|
-*	2017/03/19	wfredk	original development
+*	2017/04/07	wfredk	original development
+*	2017/04/07	wfredk	prototype dates updated
 *	2017/03/19	wfredk	prototype dates updated
 *	2017/01/04	wfredk	prototype updated to 2017
 *	2016/12/20	wfredk	original Video_Test_Fixture prototype development
