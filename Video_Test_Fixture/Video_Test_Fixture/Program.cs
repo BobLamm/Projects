308e2a9cb94fd316c5d8ac55b6464c37446033b2
Fred Koschara <github@L5Software.com>
2017-04-26 12:39:19 -0700

    Reconstructed new functionality after git overwrite

--- Video_Test_Fixture/Video_Test_Fixture/Program.cs
+++ Video_Test_Fixture/Video_Test_Fixture/Program.cs
@@ -10,10 +10,11 @@
 *
 *	Author:			Bob Lamm
 *	Creation Date:	October twentieth, 2016
-*	Last Modified:	January 9, 2017 @ 7:29 pm
+*	Last Modified:	April 26, 2017 @ 12:32 pm
 *
 *	Revision History:
 *	   Date		  by		Description
+*	2017/04/26	wfredk	reconstructed new functionality after git overwrite
 *	2017/01/09	wfredk	GlobalConfig.Init() reads Registry, not this module (restored)
 *	2016/12/20	wfredk	add Registry support, catch startup exceptions
 *	2016/12/20	wfredk	rename program to Video_Test_Fixture, add documentation
@@ -32,12 +33,25 @@ namespace Video_Test_Fixture
         [STAThread]
         static void Main()
         {   GlobalConfig cfg = GlobalConfig.Instance;
-            cfg.Init();
+            int hr;
 
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
+
+            WndSplashScreen splash = new WndSplashScreen();
+            splash.Show();
+
+            cfg.Init();
+
+            hr = cfg.MediaCtl.Run();
+            if (hr < 0)
+            {   splash.Hide();
+                MessageBox.Show("Central graph failed to start","Fatal Error");
+                return;
+            }
+
             try
-            {   Application.Run(new MainWindow());
+            {   Application.Run(new MainWindow(splash));
             }
             catch (Exception ex)
             {   MessageBox.Show(ex.ToString(), "Program Halted");
