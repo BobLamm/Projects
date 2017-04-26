de7e3ec666faadb54da8874b521fdc5a3fb183e4
Fred Koschara <github@L5Software.com>
2017-04-26 10:59:53 -0700

    Added preliminary error code documentation

--- Video_Test_Fixture/Video_Test_Fixture/VgvErrorCode.cs
+++ Video_Test_Fixture/Video_Test_Fixture/VgvErrorCode.cs
@@ -10,11 +10,11 @@
 *
 *	Author:			Fred Koschara
 *	Creation Date:	January eighth, 2017
-*	Last Modified:	April 13, 2017 @ 9:27 pm
+*	Last Modified:	April 19, 2017 @ 10:00 pm
 *
 *	Revision History:
 *	   Date		  by		Description
-*	2017/04/13	wfredk	original development
+*	2017/04/19	wfredk	original development
 *		|						|
 *	2017/01/08	wfredk	original development
 */
@@ -22,15 +22,42 @@ using System;
 
 namespace Video_Test_Fixture
 {
+    /// <summary>
+    /// error code values used in the VGV software
+    /// </summary>
     public enum VgvErrorCode : int
     {
+        /// <summary>
+        /// no error
+        /// </summary>
         NO_ERROR = 0,
+        /// <summary>
+        /// input already set
+        /// </summary>
         INPUT_ALREADY_SET,
+        /// <summary>
+        /// no input configured
+        /// </summary>
         NO_INPUT_CONFIGURED,
+        /// <summary>
+        /// end of file encountered
+        /// </summary>
         END_OF_FILE,
+        /// <summary>
+        /// no signal present
+        /// </summary>
         NO_SIGNAL,
+        /// <summary>
+        /// camera command failed
+        /// </summary>
         FAILED_CAM_COMMAND,
+        /// <summary>
+        /// add datatable row failed
+        /// </summary>
         FAILED_ADD_DATATABLE_ROW,
+        /// <summary>
+        /// create bridge filter failed
+        /// </summary>
         FAILED_CREATE_BRIDGE_FILTER
     }
 }
