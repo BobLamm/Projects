b6a54bde1a30e0a43383fa6ce4e1f4cde7fce5e8
Fred Koschara <github@L5Software.com>
2017-04-26 10:57:47 -0700

    Add splash screen

---
+++ Video_Test_Fixture/Video_Test_Fixture/WndSplashScreen.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace Video_Test_Fixture
+{
+    public partial class WndSplashScreen : Form
+    {
+        public WndSplashScreen()
+        {
+            InitializeComponent();
+
+            Show();
+
+            for (int cnt=0; cnt<333; cnt++)
+            {
+                Application.DoEvents();
+                Thread.Sleep(1);
+            }
+        }
+    }
+}
