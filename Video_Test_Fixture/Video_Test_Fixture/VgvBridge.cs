51e4bc97e3c5cd0259766ac4a07cdcf5a0c8aad8
Fred Koschara <github@L5Software.com>
2017-04-26 12:40:31 -0700

    Further VgvBridge development to improve usablity

--- Video_Test_Fixture/Video_Test_Fixture/VgvBridge.cs
+++ Video_Test_Fixture/Video_Test_Fixture/VgvBridge.cs
@@ -10,16 +10,19 @@
 *
 *	Author:			Fred Koschara
 *	Creation Date:	April seventh, 2017
-*	Last Modified:	April 10, 2017 @ 4:23 pm
+*	Last Modified:	April 26, 2017 @ 11:42 am
 *
 *	Revision History:
 *	   Date		  by		Description
-*	2017/04/10	wfredk	original development
+*	2017/04/26	wfredk	original development
 *		|						|
 *	2017/04/07	wfredk	original development
 */
 using System;
+using DirectShowLib;
 using GMFBridgeLib;
+
+using Utility.VgvUtility;
 
 namespace Video_Test_Fixture
 {
@@ -28,8 +31,16 @@ namespace Video_Test_Fixture
     /// </summary>
     public class VgvBridge
     {
-        private GlobalConfig cfg = null;
+//      private GlobalConfig cfg = null;
+//      private IBaseFilter filterIn = null;
+//      private IBaseFilter filterOut = null;
+        private IBaseFilter filterSink = null;
+        private IBaseFilter filterSrc = null;
         private IGMFBridgeController bridge = null;
+        // the sink graph, feeding out from the bridge with a source filter
+        private IGraphBuilder graphSink = null;
+        // the source graph, feeding into the bridge with a sink filter
+        private IGraphBuilder graphSrc = null;
 
         /// <summary>
         /// Gets the bridge.
@@ -44,13 +55,62 @@ namespace Video_Test_Fixture
         }
 
         // --------------------------------------------------------------------
+
+        // helper method for eliminating code duplication
+        private void CreateBridge()
+        {
+            bridge = (IGMFBridgeController)new GMFBridgeController();
+            bridge.AddStream(1,eFormatType.eUncompressed,1);
+        }
+
         /// <summary>
-        /// constructor - nitializes a new instance of the <see cref="VgvBridge"/> class.
+        /// constructor - initializes a new anonymous instance of
+        /// the <see cref="VgvBridge"/> class
         /// </summary>
         public VgvBridge()
         {
-            bridge = (IGMFBridgeController)new GMFBridgeController();
-            bridge.AddStream(1,eFormatType.eMuxInputs,1);
+            CreateBridge();
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="VgvBridge"/> class
+        /// with its output (sink filter) connected to the source graph
+        /// </summary>
+        /// <param name="graph">The graph.</param>
+        /// <param name="feedPin">The feed pin.</param>
+        public VgvBridge(IGraphBuilder graph,IPin feedPin)
+        {
+            int hr;
+
+            CreateBridge();
+
+            graphSrc = graph;
+
+            filterSink = (IBaseFilter)bridge.InsertSinkFilter(graph);
+
+//          VgvUtil.ListPins(filterSink);
+
+            // connect input pin and sink filter
+            hr = graph.ConnectDirect(feedPin,VgvUtil.GetPin(filterSink,"Input 1"),null);
+            VgvUtil.checkHR(hr,"Can't connect feed pin and output filter");
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="VgvBridge"/> class.
+        /// </summary>
+        /// <param name="destPin">The dest pin.</param>
+        /// <param name="graph">The graph.</param>
+        /// <exception cref="NotImplementedException"></exception>
+        public VgvBridge(IPin destPin,IGraphBuilder graph)
+        {
+//          int hr;
+
+            CreateBridge();
+
+            graphSink = graph;
+
+            // TODO !!
+            throw new NotImplementedException();
         }
 
         // --------------------------------------------------------------------
@@ -60,6 +120,73 @@ namespace Video_Test_Fixture
         ~VgvBridge()
         {
             // TODO
+        }
+
+        // --------------------------------------------------------------------
+
+        /// <summary>
+        /// Connects or disconnects the graphs.
+        /// 
+        /// N.B. The graphs should not be connected until the downstream graph
+        ///     has been fully constructed.
+        /// N.B. The grsphs must be disconnected before either is stopped.
+        /// </summary>
+        /// <param name="bConnect">if set to <c>true</c> [b connect].</param>
+        public void ConnectGraphs(bool bConnect=true)
+        {
+            if (bConnect) // connect the two graphs
+                bridge.BridgeGraphs(filterSink,filterSrc);
+            else bridge.BridgeGraphs(null,null);    // disconnect
+        }
+
+        /// <summary>
+        /// connects the source graph as the input to the bridge
+        ///
+        /// inserts a sink filter into the graph and connects the graph output pin
+        /// to the bridge's input (the input pin of the sink filter)
+        /// </summary>
+        /// <param name="graph">The graph.</param>
+        /// <param name="feedPin">The feed pin.</param>
+        /// <returns></returns>
+        public IBaseFilter ConnectInput(IGraphBuilder graph,IPin feedPin)
+        {
+            int hr;
+
+            graphSink = graph;
+
+            filterSink = (IBaseFilter)bridge.InsertSinkFilter(graph);
+
+            // connect feed pin and sink filter
+            hr = graph.ConnectDirect(feedPin,VgvUtil.GetPin(filterSink,"Input 1"),null);
+            VgvUtil.checkHR(hr,"Can't connect feed pin and output filter");
+
+            return filterSink;
+        }
+
+        /// <summary>
+        /// Connects the output of the bridge as the input for the downstream graph
+        /// </summary>
+        /// <param name="destPin">The dest pin.</param>
+        /// <param name="graph">The graph.</param>
+        /// <returns></returns>
+        public IBaseFilter ConnectOutput(IPin destPin,IGraphBuilder graph)
+        {
+            int hr;
+
+            graphSink = graph;
+
+            if (filterSink == null)
+                return null;
+
+            filterSrc = (IBaseFilter)bridge.InsertSourceFilter(filterSink,graph);
+
+//          VgvUtil.ListPins(filterSrc);
+
+            // connect source filter and destination pin
+            hr = graph.ConnectDirect(VgvUtil.GetPin(filterSrc,"Output 1"),destPin,null);
+            VgvUtil.checkHR(hr,"Can't connect input filter and destination pin");
+
+            return filterSrc;
         }
     }
 }
